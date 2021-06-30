91d8cef97d75f7ddda60daae354bc4059b7c69c0
zhijie <zhihuan@microsoft.com>
2021-06-30 13:27:40 +0800

    refactor: replace deprecated interface

--- packages/fx-core/src/plugins/resource/function/utils/dialog.ts
+++ packages/fx-core/src/plugins/resource/function/utils/dialog.ts
@@ -1,17 +1,19 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
-import { DialogMsg, DialogType, MsgLevel, PluginContext } from "@microsoft/teamsfx-api";
+import { PluginContext } from "@microsoft/teamsfx-api";
+
+enum MsgLevel {
+  Info = "info",
+  Warn = "warn",
+  Error = "error",
+}
 
 export class DialogUtils {
   public static async show(
     ctx: PluginContext,
     message: string,
-    level = MsgLevel.Info
+    level: MsgLevel.Info
   ): Promise<void> {
-    const content: DialogMsg = new DialogMsg(DialogType.Show, {
-      description: message,
-      level: level,
-    });
-    await ctx.dialog?.communicate(content);
+    await ctx.ui?.showMessage(level, message, false);
   }
 }
